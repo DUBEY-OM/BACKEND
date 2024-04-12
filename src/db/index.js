4f1ab7691769715e8fd5d6d36032fdd4c3e51285
DUBEY-OM <om1873004@gmail.com>
2024-04-12 20:19:54 +0530

    connection nhi mil rha m*dharch*d

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-;const ConnectDB=async ()=>{
-        try {
-           const ConnectionInstance=await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-            console.log(`\n MongoDB Connected !! DB_HOST:${ConnectionInstance,connection.host}`)
-        } catch (error) {
-            console.error("MONGO_DB CONNECTION FAILED",error);
-            process.exit(1)}
-            app.listen(process.env.PORT,()=>{console.log(`App listening on PORT ${procee.env.PORT}`)})
-        }
-        export default ConnectDB
+
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
